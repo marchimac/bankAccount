964a9692958ad2800a8da711fead2505ebdc97aa
Fernando <fernandoms1078@hotmail.com>
2023-01-29 23:13:46 +0100

    Monto el bucle recurrente para el menú

--- src/com/example/bankaccount/Main.java
+++ src/com/example/bankaccount/Main.java
@@ -1,15 +1,38 @@
 package com.example.bankaccount;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         BankAccountService bankAccountService = new BankAccountServiceImpl();
 
-        List<BankAccount> articles = bankAccountService.findAll();
-        System.out.println("Hay un total de " + articles.size() + " artículos.");
-        for (BankAccount article : articles)
-            System.out.println(article);
+        int option;
+        Scanner entrada = new Scanner(System.in);
+
+        do {
+
+            System.out.println("""
+                    Welcome to MarchiBank
+                    
+                    1. Buscar TODAS las cuentas
+                    
+                    """);
+
+            option = entrada.nextInt();
+
+            if (option == 1) {
+
+                List<BankAccount> articles = bankAccountService.findAll();
+                System.out.println("Hay un total de " + articles.size() + " artículos.");
+                for (BankAccount article : articles) {
+                    System.out.println(article);
+                }
+
+            }
+
+        } while (option != 13);
+
     }
 }
